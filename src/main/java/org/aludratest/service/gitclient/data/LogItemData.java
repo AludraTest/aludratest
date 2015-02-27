35e3b98bb65ffc8416672d8861fe2963dffc927c
Volker Bergmann <volker@databene.org>
2015-02-27 17:23:43 +0100

    Added Git Client tests and improved implementation

---
+++ src/main/java/org/aludratest/service/gitclient/data/LogItemData.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2010-2014 Hamburg Sud and the contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aludratest.service.gitclient.data;
+
+import org.aludratest.service.gitclient.GitClient;
+import org.databene.commons.SystemInfo;
+
+/** Wraps result data of the {@link GitClient}'s log method.
+ * @see GitClient#log(LogData)
+ * @author Volker Bergmann */
+public class LogItemData extends AbstractGitData {
+
+    private static final String LF = SystemInfo.getLineSeparator();
+
+    private String commit;
+    private String merge;
+    private String author;
+    private String date;
+    private String message;
+
+    /** Public default constructor. */
+    public LogItemData() {
+        this(null, null, null, null, null);
+    }
+
+    /** Full constructor.
+     * @param commit the commit message
+     * @param merge the merge info
+     * @param author the author
+     * @param date the date
+     * @param message the message */
+    public LogItemData(String commit, String merge, String author, String date, String message) {
+        setCommit(commit);
+        setMerge(merge);
+        setAuthor(author);
+        setDate(date);
+        setMessage(message);
+    }
+
+    /** Returns the commit message.
+     * @return the commit message */
+    public String getCommit() {
+        return commit;
+    }
+
+    /** Sets the commit message.
+     * @param commit the commit message */
+    public void setCommit(String commit) {
+        this.commit = commit;
+    }
+
+    /** Returns the merge info.
+     * @return the merge info */
+    public String getMerge() {
+        return merge;
+    }
+
+    /** Sets the merge info.
+     * @param merge the merge info to set */
+    public void setMerge(String merge) {
+        this.merge = merge;
+    }
+
+    /** Returns the author.
+     * @return the author */
+    public String getAuthor() {
+        return author;
+    }
+
+    /** Sets the author.
+     * @param author the author */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /** Returns the date.
+     * @return the date */
+    public String getDate() {
+        return date;
+    }
+
+    /** Sets the date.
+     * @param date the date */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /** Returns the message.
+     * @return the message */
+    public String getMessage() {
+        return message;
+    }
+
+    /** Sets the message.
+     * @param message the message */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "commit " + commit + LF + "Author: " + author + LF + "Date:   " + date + LF + LF + "    " + message;
+    }
+
+}
---
+++ src/main/java/org/aludratest/service/gitclient/data/RenamedStatusData.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2010-2014 Hamburg Sud and the contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aludratest.service.gitclient.data;
+
+import org.aludratest.service.gitclient.GitClient;
+import org.databene.commons.NullSafeComparator;
+
+/** Provides data structure for the result of the {@link GitClient}'s status method.
+ * @see GitClient#status(StatusData)
+ * @author Volker Bergmann */
+public class RenamedStatusData extends AbstractGitData {
+
+    private String fromPath;
+    private String toPath;
+
+    /** Public default constructor. */
+    public RenamedStatusData() {
+        this(null, null);
+    }
+
+    /** Full constructor.
+     * @param fromPath
+     * @param toPath */
+    public RenamedStatusData(String fromPath, String toPath) {
+        setFromPath(fromPath);
+        setToPath(toPath);
+    }
+
+    /** Returns the {@link #fromPath}
+     * @return the {@link #fromPath} */
+    public String getFromPath() {
+        return fromPath;
+    }
+
+    /** Sets the {@link #fromPath}.
+     * @param fromPath sets the {@link #fromPath} */
+    public void setFromPath(String fromPath) {
+        this.fromPath = fromPath;
+    }
+
+    /** Returns the {@link #toPath}
+     * @return the {@link #toPath} */
+    public String getToPath() {
+        return toPath;
+    }
+
+    /** Sets the {@link #toPath}
+     * @param toPath the {@link #toPath} to set */
+    public void setToPath(String toPath) {
+        this.toPath = toPath;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        if (fromPath != null) {
+            result = result * 31 + fromPath.hashCode();
+        }
+        if (toPath != null) {
+            result = result * 31 + toPath.hashCode();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        RenamedStatusData that = (RenamedStatusData) obj;
+        return NullSafeComparator.equals(this.fromPath, that.fromPath) && NullSafeComparator.equals(this.toPath, that.toPath);
+    }
+
+    @Override
+    public String toString() {
+        return fromPath + " -> " + toPath;
+    }
+
+}
