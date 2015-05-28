72e34c4aa8dadf97dd0d8bb363695ddb8dd56076
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2015-05-28 09:19:15 +0200

    Added missing license headers

--- src/main/java/org/aludratest/testcase/data/impl/xml/model/TestDataFieldType.java
+++ src/main/java/org/aludratest/testcase/data/impl/xml/model/TestDataFieldType.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2010-2014 Hamburg Sud and the contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.aludratest.testcase.data.impl.xml.model;
 
 import javax.xml.bind.annotation.XmlEnum;
@@ -7,23 +22,23 @@ import javax.xml.bind.annotation.XmlEnumValue;
 public enum TestDataFieldType {
 
     @XmlEnumValue("STRING")
-    STRING, 
-    
+    STRING,
+
     @XmlEnumValue("DATE")
-    DATE, 
-    
+    DATE,
+
     @XmlEnumValue("NUMBER")
-    NUMBER, 
-    
+    NUMBER,
+
     @XmlEnumValue("BOOLEAN")
-    BOOLEAN, 
-    
+    BOOLEAN,
+
     @XmlEnumValue("OBJECT")
-    OBJECT, 
-    
+    OBJECT,
+
     @XmlEnumValue("OBJECT_LIST")
-    OBJECT_LIST, 
-    
+    OBJECT_LIST,
+
     @XmlEnumValue("STRING_LIST")
     STRING_LIST;
 
