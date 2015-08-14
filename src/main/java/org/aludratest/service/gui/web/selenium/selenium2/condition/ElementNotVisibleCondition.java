a3a28b8c8729111f46a8e105449d92f41e6691b5
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2015-08-14 09:50:39 +0200

    Added missing condition class

---
+++ src/main/java/org/aludratest/service/gui/web/selenium/selenium2/condition/ElementNotVisibleCondition.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2010-2014 Hamburg Sud and the contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aludratest.service.gui.web.selenium.selenium2.condition;
+
+import org.aludratest.service.gui.web.selenium.selenium2.LocatorSupport;
+import org.aludratest.service.locator.element.GUIElementLocator;
+import org.openqa.selenium.WebElement;
+
+public class ElementNotVisibleCondition extends WebElementCondition {
+
+    public ElementNotVisibleCondition(GUIElementLocator locator, LocatorSupport locatorSupport) {
+        super(locator, locatorSupport);
+    }
+
+    @Override
+    protected WebElement applyOnElement(WebElement element) {
+        if (element.isDisplayed()) {
+            message = "The element is visible.";
+            return null;
+        }
+
+        return element;
+    }
+
+}
