52a4cdce44b6b3119eddee6c50d56e9cf93e3cd6
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2015-12-17 10:51:08 +0100

    Implement dry run feature

--- src/main/java/org/aludratest/scheduler/util/CommonRunnerLeafAttributes.java
+++ src/main/java/org/aludratest/scheduler/util/CommonRunnerLeafAttributes.java
@@ -25,4 +25,8 @@ public interface CommonRunnerLeafAttributes {
 
     public static final String SEQUENTIAL_GROUP_INDEX = "SEQUENTIAL_GROUP_INDEX";
 
+    /** Attribute which is set to <code>true</code> when the RunnerTreeBuilder could not build the runner leaf's invoker, e.g. due
+     * to data errors. */
+    public static final String BUILDER_ERROR = "BUILDER_ERROR";
+
 }
