eb1e4653f5e47bb543a0b6177821d0f831586fa4
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2016-01-21 14:06:42 +0100

    Remove Selenium implementation - moved to own project

--- src/main/java/org/aludratest/util/DataUtil.java
+++ src/main/java/org/aludratest/util/DataUtil.java
@@ -15,25 +15,14 @@
  */
 package org.aludratest.util;
 
-import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.aludratest.exception.AutomationException;
-import org.aludratest.service.locator.element.XPathLocator;
 import org.databene.commons.ParseUtil;
 import org.databene.commons.StringUtil;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.tidy.Tidy;
 
 /**
  * Provides data related utility methods.
@@ -122,48 +111,6 @@ public class DataUtil {
         // elements were converted to uppercase before, but it is nowhere stated that this would be expected,
         // and containsStrings() also did not convert to uppercase.
         return Arrays.asList(actualStrings).contains(expectedString) ? "" : expectedString;
-    }
-
-    private static final MostRecentUseCache.Factory<String, Document> docFactory = new MostRecentUseCache.Factory<String, Document>() {
-        @Override
-        public Document create(String html) {
-            Tidy tidy = new Tidy();
-            tidy.setQuiet(true);
-            tidy.setShowWarnings(false);
-            tidy.setShowErrors(0);
-            tidy.setXmlOut(false);
-            Document document = tidy.parseDOM(new StringReader(html), null);
-            return document;
-        }
-    };
-
-    /* Use an MRU cache for HTML -> DOM mappings. If executing several XPaths on same HTML, this increases performance
-     * significantly. */
-    private static final MostRecentUseCache<String, Document> docCache = new MostRecentUseCache<String, Document>(docFactory, 50);
-
-    public static NodeList evalXPathInHTML(XPathLocator locator, String html) {
-        return evalXPathInHTML(locator.toString(), html);
-    }
-
-    public static NodeList evalXPathInHTML(String xpath, String html) {
-        try {
-            Document document = docCache.get(html);
-            XPathExpression expression = XPathFactory.newInstance().newXPath().compile(xpath);
-            return (NodeList) expression.evaluate(document, XPathConstants.NODESET);
-        } catch (XPathExpressionException e) {
-            throw new AutomationException("Illegal XPath: " + xpath, e);
-        }
-    }
-
-    public static String evalXPathInHTMLAsString(String xpath, String html) {
-        try {
-            Document document = docCache.get(html);
-            XPathExpression expression = XPathFactory.newInstance().newXPath().compile(xpath);
-            return (String) expression.evaluate(document, XPathConstants.STRING);
-        }
-        catch (XPathExpressionException e) {
-            throw new AutomationException("Illegal XPath: " + xpath, e);
-        }
     }
 
 }
