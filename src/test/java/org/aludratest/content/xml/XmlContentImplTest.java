38d0544a213ebbcff40bc8afaae79de4fdad62f1
Volker Bergmann <Volker.Bergmann-External@hamburgsud.com>
2016-02-17 17:49:47 +0100

    Fixed linefeed handling in XML test pt2

--- src/test/java/org/aludratest/content/xml/XmlContentImplTest.java
+++ src/test/java/org/aludratest/content/xml/XmlContentImplTest.java
@@ -90,8 +90,7 @@ public class XmlContentImplTest {
     public void testQueryElementText() throws Exception {
         Document doc = parseSimpleXml();
         assertEquals("text", content.queryElementText(doc, "/root/node"));
-        String actualText = content.queryElementText(doc, "/root");
-        assertEquals("\n  text\n", actualText);
+        assertEquals("\n  text\n", content.queryElementText(doc, "/root"));
         assertNull(content.queryElementText(doc, "/root/nonode"));
     }
 
