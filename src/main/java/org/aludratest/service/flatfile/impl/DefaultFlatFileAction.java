d95c25ebc34fb0812761da4d2f7c79123500abb3
Volker Bergmann <volker@databene.org>
2016-12-13 10:20:56 +0100

    fixed equals method

--- src/main/java/org/aludratest/service/flatfile/impl/DefaultFlatFileAction.java
+++ src/main/java/org/aludratest/service/flatfile/impl/DefaultFlatFileAction.java
@@ -192,8 +192,13 @@ public class DefaultFlatFileAction implements FlatFileInteraction, FlatFileVerif
         }
 
         @Override
-        public boolean equals(Object other) {
-            return internalKey.equals(other);
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            WriterKey that = (WriterKey) obj;
+            return this.internalKey.equals(that.internalKey); // internalKey is never null
         }
 
         @Override
